9ba4358c172a73c0e3e279d5aedb2101e199aea0
Divakar V Prabhu <divaprabhu@outlook.com>
2024-11-16 11:57:30 +0530

    remove output directory

--- create_m3u.sh
+++ create_m3u.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # 0 */3 * * * cd /home/dvp/iptv && sh create_m3u.sh >> /tmp/m3u.log
-#https://t.ly/hnO52
+# python -m http.server --directory /tmp
 
 python create_m3u.py
-rclone copy /tmp/list.m3u hotmail:
